2748292f06f3ee376ae5477113b840218599951d
Sang <thanato0912@gmail.com>
2021-10-30 13:29:06 +0900

    feat: quill editior

--- front/src/TextEditor.js
+++ front/src/TextEditor.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import io from 'socket.io-client'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
 
 const TextEditor = () => {
   const [socket, setSocket] = useState(null)
@@ -13,23 +15,65 @@ const TextEditor = () => {
     return () => newSocket.close()
   }, [])
 
+  const handleChange = value => {
+    setMsg(value)
+  }
+
   useEffect(() => {
     if (msg !== '') {
       socket.emit('message', msg)
     }
   }, [msg])
 
+  const modules = {
+    toolbar: [
+      //[{ 'font': [] }],
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [
+        { list: 'ordered' },
+        { list: 'bullet' },
+        { indent: '-1' },
+        { indent: '+1' }
+      ],
+      ['link', 'image'],
+      [{ align: [] }, { color: [] }, { background: [] }], // dropdown with defaults from theme
+      ['clean']
+    ]
+  }
+
+  const formats = [
+    //'font',
+    'header',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'indent',
+    'link',
+    'image',
+    'align',
+    'color',
+    'background'
+  ]
+
   // socket?.on('connect', console.log(socket))
 
   return (
-    <div>
-      <h3>Here's the TextEdit component.</h3>
-      <textarea
-        rows='10'
-        cols='50'
-        placeholder='Write something here...'
-        onChange={e => setMsg(e.target.value)}
-      ></textarea>
+    <div style={{ height: '650px' }}>
+      <ReactQuill
+        style={{ height: '600px' }}
+        theme='snow'
+        modules={modules}
+        formats={formats}
+        value={msg || ''}
+        onChange={(content, delta, source, editor) =>
+          handleChange(editor.getHTML())
+        }
+      />
     </div>
   )
 }
